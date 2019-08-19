f71171b55ea75526b34acc9fb1fa381ff9100167
BorisTab <boristab23@gmail.com>
2019-08-19 10:55:00 +0300

    add conversion image to base64

--- static/scripts/auth.js
+++ static/scripts/auth.js
@@ -31,8 +31,13 @@ $(document)
             } else {
                 delete data['confirm_password'];
                 $('#toBase64').attr('src', data.image);
-                // data.image = getBase64ImageById('toBase64');
-                console.log($('#file').prop('files'));
+                const reader = new FileReader();
+                reader.onload = function(e){
+                    const arrayBuffer = e.target.result;
+                    data.image = arrayBuffer;
+                };
+                reader.readAsDataURL($('#file').prop('files')[0]);
+                console.log($('#file').prop('files')[0]);
                 fetch('/req/reg', {
                     method: 'POST',
                     body: JSON.stringify(data),
@@ -44,22 +49,32 @@ $(document)
         console.log(data);
     });
 
-function getBase64Image(img) {
-    // создаем канвас элемент
-    const canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
+// document.querySelector('#file').addEventListener('change', function(){
+//     var reader = new FileReader();
+//     reader.onload = function(){
+//         var arrayBuffer = this.result;
+//         console.log(arrayBuffer);
+//         document.querySelector('#result').innerHTML = arrayBuffer + '  '+arrayBuffer.byteLength;
+//     };
+//     reader.readAsArrayBuffer(this.files[0]);
+// }, false);
 
-    // Копируем изображение на канвас
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-
-    // Получаем data-URL отформатированную строку
-    // Firefox поддерживает PNG и JPEG.
-    const dataURL = canvas.toDataURL("image/png");
-
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
-function getBase64ImageById(id){
-   return getBase64Image(document.getElementById(id));
-}
+// function getBase64Image(img) {
+//     // создаем канвас элемент
+//     const canvas = document.createElement("canvas");
+//     canvas.width = img.width;
+//     canvas.height = img.height;
+//
+//     // Копируем изображение на канвас
+//     const ctx = canvas.getContext("2d");
+//     ctx.drawImage(img, 0, 0);
+//
+//     // Получаем data-URL отформатированную строку
+//     // Firefox поддерживает PNG и JPEG.
+//     const dataURL = canvas.toDataURL("image/png");
+//
+//     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+// }
+// function getBase64ImageById(id){
+//    return getBase64Image(document.getElementById(id));
+// }
