eb6d19573613a21cf997fd350178a0c783e7bc92
naumtsev <naumtsevanton@mail.ru>
2019-08-20 09:39:21 +0300

    change some func

--- assessment_functions.py
+++ assessment_functions.py
@@ -1,13 +1,30 @@
 def update_user_recommendation(user, posts):
     result_data = user['result_data']
-    result_recommendation = user['result_recommendation']
-
+    result_recommendation = dict()
     # Редактирование result data, result_recomendation, posts - это оценкци пользователей
 
+    # posts  [post1, post2, post3]
+
+    # post:
+    #     post_id - 4,
+    #     author_id - 2,
+    #     appreciated_id - 3,
+    #     group_id = 4,
+    #     date = '2222-01-12', # year, month, day
+    #     parameters = {
+    #           'par1':1,
+    #           'par2':1,
+    #           'par2': 2,
+    #     }
+
+    for post in posts:
+        post['parameters']['par1'] = 1
 
 
-    #
+
 
     user['result_data'] = result_data
-    user['result_recommendation'] = result_recommendation
+    user['result_recommendation'] = {'Стрессоустойчивость': 'Относитесь к вещам с улыбкой',
+     'Общение': 'поменьше мата в речи'}
+
     return user
